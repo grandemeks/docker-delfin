ea3bd099a5ca46091129f0e521a87ea5f0afa2f2
grandemeks <grandemeks@gmail.com>
2025-07-19 12:34:50 +0200

    Added .env and requirements.txt file

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,20 @@
-
-
 # Use slim Python image as base image
 FROM python:3.11-slim
 
 # Set working directory inside the container
 WORKDIR /app
 
-# Copy flask app into the container
-COPY app.py .
+# Copy requirements file
+COPY requirements.txt requirements.txt
 
-#Instal required Python packages directly
-RUN pip install Flask psycopg2-binary
+# Install Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy entire application (including app.py, .env, etc.)
+COPY . .
 
 # Expose the port the app runs on
 EXPOSE 8080
-
 
 # Start the app
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,25 +1,15 @@
-from flask import Flask # Web framework for Python
-import psycopg2 # PostgreSQL database adapter for Python (client library)
-import os # Access to environment variables
+from flask import Flask
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Read WELCOME_MESSAGE from .env (with fallback)
+message = os.getenv("WELCOME_MESSAGE", "Hello from fallback!")
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    try:
-        # Try to connect to the PostgreSQL using env variables
-
-        conn = psycopg2.connect(
-            host=os.environ.get('DB_HOST', 'localhost'),
-            dbname=os.environ.get('DB_NAME'),
-            user=os.environ.get('DB_USER'),
-            password=os.environ.get('DB_PASS')
-        )
-        return "✅ Connected to PostgreSQL!"
-    except Exception as e:
-        # If connection fails, show error message
-        return f"❌ PostgreSQL connection failed: {e}"
-    
-if __name__ == '__main__':
-    # Start Flask app on 0.0.0.0:8080 so it's accesible from Docker
-    app.run(host="0.0.0.0", port=8080)
+    return f"<h1>{message}</h1>"
